b465dbed417c3324ed3dbb355ec74626db21c573
PUTERA NAQIB KHUSAIRI BIN ASRI <130439246+NaqibKhusairy@users.noreply.github.com>
2023-10-30 22:32:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                     <input type="text" placeholder="Name" name="name" required />
                     <input type="text" placeholder="Username" name="username" required />
                     <input type="text" placeholder="Phone Number" name="phone" required />
-                    <input type="email" placeholder="Email" name "email" required />
+                    <input type="email" placeholder="Email" name="email" required />
                     <input type="password" placeholder="Password" name="password" id="password" required />
                     <input type="checkbox" id="showPassword"> Show Password
                     <input type="hidden" name="process" value="register" required />
@@ -37,50 +37,5 @@
         </div>
     </div>
     <script src="javascript.js"></script>
-    <script>
-        var loginButton = document.getElementById("loginButton");
-        var registerButton = document.getElementById("registerButton");
-        var loginForm = document.forms["loginForm"];
-        var registerForm = document.forms["registerForm"];
-
-        loginButton.onclick = function () {
-            document.querySelector("#flipper").classList.toggle("flip");
-            resetForm(loginForm);
-        };
-
-        registerButton.onclick = function () {
-            document.querySelector("#flipper").classList.toggle("flip");
-            resetForm(registerForm);
-        };
-
-        // Show/hide password function
-        function togglePasswordVisibility() {
-            var passwordFields = document.getElementsByName("password");
-            for (var i = 0; i < passwordFields.length; i++) {
-                if (passwordFields[i].type === "password") {
-                    passwordFields[i].type = "text";
-                } else {
-                    passwordFields[i].type = "password";
-                }
-            }
-        }
-
-        var passwordInputs = document.getElementsByName("password");
-        if (passwordInputs.length > 0) {
-            var showPasswordCheckboxes = document.querySelectorAll('#showPassword');
-            showPasswordCheckboxes.forEach(function (checkbox) {
-                checkbox.addEventListener("change", togglePasswordVisibility);
-            });
-        }
-
-        // Function to reset form fields
-        function resetForm(form) {
-            form.reset();
-            var passwordFields = document.getElementsByName("password");
-            for (var i = 0; i < passwordFields.length; i++) {
-                passwordFields[i].type = "password"; // Change input type to password
-            }
-        }
-    </script>
 </body>
 </html>
